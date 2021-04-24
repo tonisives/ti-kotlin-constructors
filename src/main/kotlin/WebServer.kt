e4b826c770f93c162b8613966ac0dd42c3f7afb2
tonis <tonisives@gmail.com>
2021-04-24 15:01:09 +0700

    step 3

--- src/main/kotlin/WebServer.kt
+++ src/main/kotlin/WebServer.kt
@@ -3,6 +3,22 @@ class WebService(val url: String, apiVersion: String = "2") {
 
     init {
         println("Initialised with api version: $apiVersion")
+        println("url: $completeUrl")
+    }
+
+    constructor(environment: Environment) : this(getUrl(environment))
+
+    enum class Environment {
+        PROD, TEST
+    }
+
+    companion object {
+        private fun getUrl(environment: Environment): String {
+            return when (environment) {
+                Environment.PROD -> "https://tonisives.com"
+                Environment.TEST -> "https://tonisives.com/test"
+            }
+        }
     }
 
     fun getUsers() {
@@ -13,4 +29,5 @@ class WebService(val url: String, apiVersion: String = "2") {
 
 fun main(args: Array<String>) {
     WebService("https://tonisives.com")
+    WebService(WebService.Environment.TEST)
 }
